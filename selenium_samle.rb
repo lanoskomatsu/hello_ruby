c0561fc5c56cffd2d3a74b93d038851e4e350bd9
lanoskomatsu <yusuke.komatsu@coincheck.com>
2018-09-02 08:51:37 +0900

    add comment

--- selenium_samle.rb
+++ selenium_samle.rb
@@ -1,13 +1,17 @@
 require 'selenium-webdriver'
 
+# ブラウザの指定
 driver = Selenium::WebDriver.for :chrome
-
+# URLにジャンプ
 driver.navigate.to 'https://www.google.co.jp/imghp?hl=ja&tab=wi&authuser=0'
 
+# 検索フォームへの入力
 element = driver.find_element(:name, 'q')
 element.send_keys('エンジニアブーツ redwing')
 
+# 検索ボタンの押下
 element = driver.find_element(:name, 'btnG')
 element.click
 
+# ブラウザを閉じる
 driver.quit
