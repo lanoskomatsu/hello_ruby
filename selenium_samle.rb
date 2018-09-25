50f4f8745bbb98464fab88a2cd5e3075070bb93f
lanoskomatsu <yusuke.komatsu@coincheck.com>
2018-09-25 23:50:00 +0900

    add

--- selenium_samle.rb
+++ selenium_samle.rb
@@ -1,4 +1,5 @@
 require 'selenium-webdriver'
+require 'pp'
 
 # ブラウザの指定
 driver = Selenium::WebDriver.for :chrome
@@ -15,21 +16,27 @@ element.click
 
 break_count = 0
 while 1 do
-  # javascriptを実行してページの最下部へ移動
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-  sleep 3
-
   begin
     # "結果をもっと表示"ボタンの検出
     if driver.find_element(:id, "smb").size != 0
       element = driver.find_element(:id, "smb")
       element.click
+      break
     end
   rescue => e
     break_count += 1
     break if break_count == 5
   end
+
+  # javascriptを実行してページの最下部へ移動
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  sleep 3
 end
+
+# id: "search" を探す
+elements = driver.find_element(:id, "search").find_elements(:tag_name, "img")
+
+pp elements.map{|e| e.attribute('src')}
 
 # ブラウザを閉じる
 driver.quit
