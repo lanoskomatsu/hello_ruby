5668719de07898e5fc164b6b256096e4b0c3153b
lanoskomatsu <yusuke.komatsu@coincheck.com>
2018-11-25 13:37:58 +0900

    add map sample.

---
+++ map_sample.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	countMap := map[string]int{}
+
+	for _, word := range strings.Fields(s) {
+		countMap[word]++
+	}
+
+	return countMap
+}
+
+func main() {
+	wc.Test(WordCount)
+}
