867d94ec8dfba8fccad10bcf68b0d7bb5e91c66f
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-02-11 23:24:41 +0900

    add

---
+++ while.py
@@ -0,0 +1,14 @@
+from random import randint
+tickets=5
+point=0
+fmt="{:>3}"
+
+while tickets>0:
+    v=randint(1,20)
+    print(fmt.format(v))
+    point += v
+    tickets -= 1
+
+print("-" * 3)
+print(fmt.format(point))
+
