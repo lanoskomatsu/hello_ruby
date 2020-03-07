e22272d7db86191db2cedbbe2a2c7c4ff839e504
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-03-07 23:29:12 +0900

    add

---
+++ python_study_samples/pyplot_legend.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+X = [100, 200, 300, 400, 500]
+Y1 = [40, 65, 80, 100, 90]
+Y2 = [34, 56, 75, 91, 79]
+Y3 = [25, 47, 68, 76, 73]
+Y4 = [15, 40, 52, 64, 69]
+plt.plot(X, Y1, marker="o", color="blue", linestyle="-", label="Y1")
+plt.plot(X, Y2, marker="v", color="red", linestyle="--", label="Y2")
+plt.plot(X, Y3, marker="^", color="green", linestyle="-.", label="Y3")
+plt.plot(X, Y4, marker="d", color="m", linestyle=":", label="Y4")
+plt.legend(loc = "upper left")
+plt.show()
