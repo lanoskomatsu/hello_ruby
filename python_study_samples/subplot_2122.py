77cb942b663c4483353d3ca10d307ecfaa4c6f89
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-03-14 22:35:43 +0900

    add

---
+++ python_study_samples/subplot_2122.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+X1, Y1 = range(0, 5), [61, 45, 27, 88, 47]
+X2, Y2 = range(0, 5), [17, 39, 46, 40, 27]
+X3, Y3 = range(0, 5), [36, 29, 48, 21, 11]
+labels = ["A", "B", "C", "D", "E"]
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 1, 1)
+ax1.bar(X1, Y1, color = "b", tick_label = labels)
+ax1.set_title("dog")
+ax2 = fig.add_subplot(2, 2, 3)
+ax2.bar(X2, Y2, color = "g", tick_label = labels)
+ax2.set_title("cat")
+ax3 = fig.add_subplot(2, 2, 4)
+ax3.bar(X3, Y3, color = "c", tick_label = labels)
+ax3.set_title("bird")
+plt.tight_layout()
+plt.show()
