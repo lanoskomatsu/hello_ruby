59a9b5537c64989429e6f2d91d0cb410ab8c4ee9
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-04-05 22:45:49 +0900

    add

---
+++ python_study_samples/numpy_slice.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+ndarray = np.array([[1, 2], [3, 4], [5, 6]])
+print(ndarray)
+
+print(ndarray[1][0])
+print(ndarray[1, 0])
+
+array = np.array([1, 2, 3, 4, 5, 6, 7])
+print(array[1:6])
+print(array[1:6:2])
