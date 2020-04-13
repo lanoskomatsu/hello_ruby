f3a41ea5e1371ed37b9ed4cd097a274e9143bd7f
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-04-14 01:12:19 +0900

    add

--- github_api_sample.rb
+++ github_api_sample.rb
@@ -3,27 +3,37 @@ require 'uri'
 require 'json'
 require 'pp'
 
-def http_request(url)
-  uri = URI.parse(url)
-  http = Net::HTTP.new(uri.hostname, uri.port)
-  http.use_ssl = (uri.scheme == 'https')
-  req = Net::HTTP::Get.new(uri.request_uri)
-  http.request(req)
+class Request
+  attr_reader :url
+  def initialize(url)
+    @url = url
+  end
+
+  def http_request
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    req = Net::HTTP::Get.new(uri.request_uri)
+    http.request(req)
+  end
 end
 
 user_name = ARGV[0]
 host = "https://api.github.com"
-query = "/users/#{user_name}/repos"
+query = "/users/#{user_name}/repos?type=all"
 
 repos = []
-res = http_request(host+query)
-repos.push(JSON.parse(res.body).map{|a| a["name"]})
+res = Request.new(host+query).http_request
+repos.concat(JSON.parse(res.body).map{|a| a["name"]})
+next_url = true
 
-next_url = res.header["link"].split(";").map{|a| a.scan(/https:\/\/.*page=\d/) }.flatten
-next_url.each{|url|
-  res = http_request(url)
-  repos.push(JSON.parse(res.body).map{|a| a["name"]})
-}
+while next_url
+  next_url = res.header["link"].scan(/\<([^<>]+)\>; rel\="next"/)[0]
+  next_url.each{|url|
+    res = Request.new(url).http_request
+    repos.concat(JSON.parse(res.body).map{|a| a["name"]})
+  }
+end
 
-pp repos.flatten.size
-pp repos.flatten
+pp repos.size
+pp repos
