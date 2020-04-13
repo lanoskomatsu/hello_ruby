e1d6dd462f0456a0c6b286bb2dba575f728b799a
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-04-14 00:17:02 +0900

    add

--- github_api_sample.rb
+++ github_api_sample.rb
@@ -3,16 +3,27 @@ require 'uri'
 require 'json'
 require 'pp'
 
+def http_request(url)
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.hostname, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+  req = Net::HTTP::Get.new(uri.request_uri)
+  http.request(req)
+end
+
 user_name = ARGV[0]
 host = "https://api.github.com"
-query = "/users/#{user_name}"
-uri = URI.parse(host+query)
+query = "/users/#{user_name}/repos"
 
-http = Net::HTTP.new(uri.hostname, uri.port)
-http.use_ssl = (uri.scheme == 'https')
-req = Net::HTTP::Get.new(uri.request_uri)
-res = http.request(req)
+repos = []
+res = http_request(host+query)
+repos.push(JSON.parse(res.body).map{|a| a["name"]})
 
-pp res.header
+next_url = res.header["link"].split(";").map{|a| a.scan(/https:\/\/.*page=\d/) }.flatten
+next_url.each{|url|
+  res = http_request(url)
+  repos.push(JSON.parse(res.body).map{|a| a["name"]})
+}
 
-pp JSON.parse(res.body)
+pp repos.flatten.size
+pp repos.flatten
