0dfe67ae1ad1fb5f09a254bc292879ef67dc5734
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-04-14 13:41:47 +0900

    add

--- github_api_sample.rb
+++ github_api_sample.rb
@@ -16,6 +16,23 @@ class Request
     req = Net::HTTP::Get.new(uri.request_uri)
     http.request(req)
   end
+
+  def repos
+    repos = []
+    res = http_request
+    repos.concat(JSON.parse(res.body).map{|a| a["name"]})
+    next_url = true
+
+    while next_url
+      next_url = res.header["link"].scan(/\<([^<>]+)\>; rel\="next"/)[0]
+      next_url&.each{|url|
+        res = Request.new(url).http_request
+        repos.concat(JSON.parse(res.body).map{|a| a["name"]})
+      }
+    end
+
+    repos
+  end
 end
 
 user_name = ARGV[0]
@@ -23,17 +40,7 @@ host = "https://api.github.com"
 query = "/users/#{user_name}/repos?type=all"
 
 repos = []
-res = Request.new(host+query).http_request
-repos.concat(JSON.parse(res.body).map{|a| a["name"]})
-next_url = true
+res = Request.new(host+query).repos
 
-while next_url
-  next_url = res.header["link"].scan(/\<([^<>]+)\>; rel\="next"/)[0]
-  next_url.each{|url|
-    res = Request.new(url).http_request
-    repos.concat(JSON.parse(res.body).map{|a| a["name"]})
-  }
-end
-
-pp repos.size
-pp repos
+pp res.size
+pp res
