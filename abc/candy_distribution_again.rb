ae6a730466907c76c8e51b6eb1ce208b402e444e
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-06-13 23:17:58 +0900

    add

---
+++ abc/candy_distribution_again.rb
@@ -0,0 +1,17 @@
+n, x = gets.split(" ").map(&:to_i)
+a = gets.split(" ").map(&:to_i).sort
+
+n.times do |i|
+  x = x - a[i]
+  if x > 0
+    next
+  else
+    if x == 0
+      puts n + 1
+    elsif x < 0
+      puts i
+    end
+    exit
+  end
+end
+puts x == 0 ? n : n - 1
