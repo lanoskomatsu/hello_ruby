17f5eb7ec55ba4feb3de9d37aeee976697f8d1af
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-07-20 00:01:54 +0900

    add

--- practical_object_oriented_design_in_ruby/chapter5/5-1.rb
+++ practical_object_oriented_design_in_ruby/chapter5/5-1.rb
@@ -1,10 +1,19 @@
 class Trirp
-  attr_reader :bocycles, :customers, :vehicle
+  attr_reader :bicycles, :customers, :vehicle
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(prepares)
+    prepares.each{|prepare|
+      case prepare
+      when Mechanic
+        prepare.prepare_bicycles(bicycles)
+      when TripCordinator
+        prepare.buy_food(customers)
+      when Driver
+        prepare.gas_up(vehicle)
+        prepare.fill_water_tank(vehicle)
+      end
+    }
   end
-
   #...
 end
 
@@ -15,5 +24,21 @@ class Mechanic
 
   def prepare_bicycle(bicycles)
     # ...
+  end
+end
+
+class TripCordinator
+  def buy_food(customers)
+    #...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    #...
+  end
+
+  def fill_water_tank(vehicle)
+    #...
   end
 end
