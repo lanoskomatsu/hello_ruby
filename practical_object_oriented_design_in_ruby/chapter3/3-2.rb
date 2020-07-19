b0800ae16c15188e77a1d661c6c10a782ef718b6
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-07-19 14:39:46 +0900

    add

--- practical_object_oriented_design_in_ruby/chapter3/3-2.rb
+++ practical_object_oriented_design_in_ruby/chapter3/3-2.rb
@@ -1,13 +1,13 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, wheel)
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
+    @wheel = Wheel.new(rim, tire)
   end
 
   def gear_inches
-    ratio * wheel.diameter
+    ratio * @wheel.diameter
   end
 
   def ratio
@@ -27,4 +27,4 @@ class Wheel
   end
 end
 
-p Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+p Gear.new(52, 11, 26, 1.5).gear_inches
